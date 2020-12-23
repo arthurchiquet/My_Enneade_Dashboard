09f8e7c76279a58c634b7f602d771905635c0ad9
Arthur Chiquet <arthur.chiquet@gmail.com>
2020-12-23 23:23:08 +0100

    deploy

--- server.py
+++ server.py
@@ -8,7 +8,15 @@ from flask_login import LoginManager, UserMixin
 # from flask_caching import Cache
 from user_mgmt import db, User as base
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(__name__,
+    external_stylesheets=[dbc.themes.DARKLY],
+    meta_tags=[
+        {
+            'name': 'viewport',
+            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'
+        }
+    ]
+)
 
 # cache = Cache(app.server, config={
 #     'CACHE_TYPE': 'filesystem',
